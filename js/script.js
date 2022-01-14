cdaaf7dfbd3717afb7a839ffb97e4ef6cfd2bb33
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-14 16:55:44 +0100

    add autoplay 3000

--- js/script.js
+++ js/script.js
@@ -7,27 +7,33 @@ new Vue ({
     name: 'Slider',
     data: {
         images: [
-            {
-                image: '://images.unsplash.com/photo-1517697471339-4aa32003c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
-            },
-            {
-                image: 'https://images.unsplash.com/photo-1459908676235-d5f02a50184b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-            },
-            {
-                image: 'https://images.unsplash.com/photo-1642106520907-a7e77f7738ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-            },
-            {
-                image: 'https://images.unsplash.com/photo-1642097538727-a3ff8624258e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-            }
+            'https://images.unsplash.com/photo-1452802447250-470a88ac82bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',    
+            'https://images.unsplash.com/photo-1459908676235-d5f02a50184b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+            'https://images.unsplash.com/photo-1503682579489-1a53aff795af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+            'https://images.unsplash.com/photo-1460398495418-62c9b5d79fbf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
         ],
         currentIndex: 0,
+        timer: null,
     },
+    // mounted: function () { //------AUTOPLAY CON TIMER
+    //     this.startRotation();
+    // },
     methods: {
         prevImage: function () {
             this.currentIndex--;
+            if (this.currentIndex < 0) {
+                this.currentIndex = this.images.length - 1;
+            }
         },
         nextImage: function () {
             this.currentIndex++;
+            if (this.currentIndex >= this.images.length) {
+                this.currentIndex = 0;
+            }
         },
+        // startRotation: function() { //------ AUTOPLAY CON TIMER
+        // this.timer = setInterval(this.nextImage, 3000);
+        // },
+
     }
 });
