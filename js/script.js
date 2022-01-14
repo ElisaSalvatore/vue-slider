0a6c388304a20df110f3cb2b2c9115c3427c80c0
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-14 15:57:52 +0100

    slider structure (changes on html and vue.js)

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@
 
 new Vue ({
     el: '#app',
+    name: 'Slider',
     data: {
-        currentIndex: 0,
-        sliderImages: [
+        images: [
             {
-                image: 'ciao://images.unsplash.com/photo-1517697471339-4aa32003c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
+                image: '://images.unsplash.com/photo-1517697471339-4aa32003c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
             },
             {
-                image: 'https://images.unsplash.com/photo-1642155549055-ff351114a8a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+                image: 'https://images.unsplash.com/photo-1459908676235-d5f02a50184b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
             },
             {
                 image: 'https://images.unsplash.com/photo-1642106520907-a7e77f7738ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
@@ -19,7 +19,8 @@ new Vue ({
             {
                 image: 'https://images.unsplash.com/photo-1642097538727-a3ff8624258e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
             }
-        ]
+        ],
+        currentIndex: 0,
     },
     methods: {
         prevImage: function () {
